6dba77d2308dad7fbf5994ea3f6e67eaa9363000
Paulo A Pereira <odesassossegado@gmail.com>
2015-04-09 20:16:23 +0100

    Refatored API router.

--- lib/todo/api.ex
+++ lib/todo/api.ex
@@ -7,41 +7,36 @@ defmodule Todo.Api do
       %{ tasks: "All tasks" } |> json
     end
 
-    desc "get a task by id"
-    route_param :id do
-      get do
-        %{ task: params[:id] } |> json
-      end
-    end
-
     desc "creates a task"
     params do
       requires :description, type: String
-      requires :completed, type: Boolean
+      requires :completed, type: Boolean, default: false
     end
     post do
       %{description: params[:description], completed: params[:completed], operation: "create"} |> json
     end
 
-    desc "updates a task"
-    params do
-      requires :id, type: Integer
-      optional :completed, type: Boolean
-      optional :description, type: String
-      at_least_one_of [:completed, :description]
-    end
-    put do
-      %{id: params[:id], description: params[:description], completed: params[:completed], operation: "update"} |> json
-    end
+    route_param :id do
+      desc "get a task by id"
+      get do
+        %{ task: params[:id] } |> json
+      end
 
-    desc "deletes a task"
-    params do
-      requires :id, type: Integer
-    end
-    delete do
-      %{id: params[:id]} |> json
-    end
+      desc "updates a task"
+      params do
+        optional :completed, type: Boolean
+        optional :description, type: String
+        at_least_one_of [:completed, :description]
+      end
+      put do
+        %{id: params[:id], description: params[:description], completed: params[:completed], operation: "update"} |> json
+      end
 
+      desc "deletes a task"
+      delete do
+        %{id: params[:id]} |> json
+      end
+    end
   end
 
   def error(conn, _e) do
